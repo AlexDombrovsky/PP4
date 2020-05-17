f9f032e1e05b4d06df3405880531a003807202b7
Alexander Dombrovsky <alexxxdoxxx@gmail.com>
2020-05-17 18:44:16 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PP_lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factorials = new List<int>();
+            var task = new Task(() =>
+            {
+                int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8 };
+                factorials = numbers.AsParallel().Select(x => Factorial(x)).ToList();
+            });
+            task.Start();
+            task.Wait();
+            foreach (var n in factorials)
+                Console.WriteLine(n);
+            Console.ReadLine();
+        }
+
+        static int Factorial(int x)
+        {
+            int result = 1;
+
+            for (int i = 1; i <= x; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine($"Факториал числа {x} равен {result}");
+            return result;
+        }
+    }
+}
